3f73d213169d523105c4c7165b658414c86dfd53
VasiliySherstnev <root@kola-nn.com>
2021-04-21 13:44:29 +0300

    feat: add login function

---
+++ src/components/catalog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './mainPage.css';
+
+const Catalog = () => (
+    <div></div>
+);
+
+export default Catalog;
+
--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,71 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './mainPage.css';
+import axios from 'axios';
 
-const MainPage = () => (
-  <div className="wrap">
-    <div className="cantainer-reg">
-      <h1>Рады видеть вас снова!</h1>
-      <h2>Введите свой пароль и Email</h2>
-    </div>
-    <form className="reg-form cantainer-reg">
-      <input className="input" type="email" placeholder="Email" />
-      <input className="input" type="password" placeholder="Password" />
-      <p className="forgot">Забыли пароль?</p>
-      <input type="submit" className="sign-in" value="Войти" />
-      <div className="no-accaunt-wrap">
-        <p className="no-accaunt">Нет аккаунта?</p>
-        <ul className="reistration">
+const MainPage = () => {
+  const [login, setLogin] = useState('');
+
+  const loginFunc = async (props) => {
+    let authUser = {};
+    try {
+      const response = await axios.get(
+        'http://jsonplaceholder.typicode.com/users'
+      );
+      console.log(response.data);
+      for (let i = 0; i < response.data.length; i++) {
+        if (response.data[i].email === props) {
+          authUser = response.data[i];
+          alert(`Welcome! ${authUser.name}`);
+        }
+      }
+      if (authUser.id) {
+        console.log(authUser);
+      } else {
+        alert('Auth FAIL');
+      }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const clickHandler = (event) => {
+    event.preventDefault();
+    if (login === '') {
+      alert('Поля не должны быть пустыми...');
+    } else {
+      loginFunc(login);
+      setLogin('');
+    }
+  };
+
+  return (
+    <div className="wrap">
+      <div className="cantainer-reg">
+        <h1>Рады видеть вас снова!</h1>
+        <h2>Введите свой пароль и Email</h2>
+      </div>
+      <form className="reg-form cantainer-reg" onSubmit={clickHandler}>
+        <input
+          onChange={(event) => setLogin(event.target.value)}
+          value={login}
+          className="input"
+          type="text"
+          placeholder="Email"
+        />
+        <input className="input" type="password" placeholder="Password" />
+        <p className="forgot">Забыли пароль?</p>
+        <input type="submit" className="sign-in" value="Войти" />
+        <div className="no-accaunt-wrap">
+          <p className="no-accaunt">Нет аккаунта?</p>
+          <ul className="reistration">
+            <li>
+              <a>Зарегистрируйтесь</a>
+            </li>
+          </ul>
+        </div>
+      </form>
+      <div className="cantainer-reg">
+        <ul className="media-social">
           <li>
-            <a>Зарегистрируйтесь</a>
+            <a
+              href="https://www.google.ru/"
+              target="_blank"
+              className="media-google"
+              rel="noreferrer"
+            >
+              <span />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://ru-ru.facebook.com/"
+              target="_blank"
+              className="media-facebook"
+              rel="noreferrer"
+            >
+              <span />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://vk.com/"
+              target="_blank"
+              className="media-vk"
+              rel="noreferrer"
+            >
+              <span />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.apple.com/ru/"
+              target="_blank"
+              className="media-apple"
+              rel="noreferrer"
+            >
+              <span />
+            </a>
           </li>
         </ul>
       </div>
-    </form>
-    <div className="cantainer-reg">
-      <ul className="media-social">
-        <li>
-          <a
-            href="https://www.google.ru/"
-            target="_blank"
-            className="media-google"
-            rel="noreferrer"
-          >
-            <span />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://ru-ru.facebook.com/"
-            target="_blank"
-            className="media-facebook"
-            rel="noreferrer"
-          >
-            <span />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://vk.com/"
-            target="_blank"
-            className="media-vk"
-            rel="noreferrer"
-          >
-            <span />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.apple.com/ru/"
-            target="_blank"
-            className="media-apple"
-            rel="noreferrer"
-          >
-            <span />
-          </a>
-        </li>
-      </ul>
+      <div className="wrap-bg" />
     </div>
-    <div className="wrap-bg" />
-  </div>
-);
+  );
+};
 
 export default MainPage;
